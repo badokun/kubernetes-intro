42afcf3f33b22085ea7a1be771f25d1eb5d014b9
Henno van Rensburg <hennovanrensburg@gmail.com>
2020-06-17 14:59:37 +0900

    Following along * Create deployments * Included a WebApi project that returns env variables on the PING method call

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# https://hub.docker.com/_/microsoft-dotnet-core
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
+WORKDIR /source
+
+# copy csproj and restore as distinct layers
+COPY sampleApp/WebApp/*.csproj .
+RUN dotnet restore
+
+# copy everything else and build app
+COPY sampleApp/WebApp/. .
+WORKDIR /source
+RUN dotnet publish -c Release -o out
+
+# Build runtime image
+# FROM mcr.microsoft.com/dotnet/core/runtime:3.1
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
+WORKDIR /source
+COPY --from=build /source/out .
+EXPOSE 80
+ENTRYPOINT ["dotnet", "WebApp.dll"]
+
+# docker build --pull --rm -f "Dockerfile" -t webapp:v1 "."
+# docker push badokun/webapp:v1
---
+++ sampleApp/WebApp/Controllers/PingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            const string version = "V2";
+            return Ok(new
+            {
+                version,
+                nodeName = Environment.GetEnvironmentVariable("MY_NODE_NAME"),
+                podName = Environment.GetEnvironmentVariable("MY_POD_NAME"),
+                podNamespace = Environment.GetEnvironmentVariable("MY_POD_NAMESPACE"),
+                podId = Environment.GetEnvironmentVariable("MY_POD_IP")
+            });
+        }
+    }
+     
+}
